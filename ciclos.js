433f33b7e1220d126617bcf726fe3649b7f8287c
IvanGodinez21 <ivano.godinez@gmail.com>
2019-10-29 19:16:16 -0600

    Se añadieron ejercisios 1 y 2, junto con estilos

---
+++ ciclos.js
@@ -0,0 +1,34 @@
+//Ejercicio 01 Del 10 al 50
+
+var btn1 = document.querySelector("#btn1")
+
+btn1.addEventListener("click", () => {
+   let select1 = document.querySelector("#select1")
+    for (let i = 10; i <= 50; i++) {
+        let nvaOpcion = document.createElement("option")
+
+        nvaOpcion.value = i
+        nvaOpcion.text = i
+
+        select1.add(nvaOpcion)
+    }
+})
+
+//Ejercicio 01 Del 100 al 80
+
+var btn2 = document.querySelector("#btn2")
+
+btn2.addEventListener("click", () => {
+    let listas2 = document.querySelector("#listas2")
+    let i = 100
+
+    while (i >= 80) {
+        let nvoElemento = document.createElement("li")
+        nvoElemento.value = i
+        nvoElemento.textContent = i
+
+        listas2.appendChild(nvoElemento)
+
+        i--
+    }
+})
